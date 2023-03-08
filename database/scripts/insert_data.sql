b11c966b3970ddc82787a28871f4daf689da5cc4
Yijungu <wnsrn4970@naver.com>
2023-03-08 19:43:30 +0900

    create table

--- database/scripts/insert_data.sql
+++ database/scripts/insert_data.sql
@@ -1,3 +1,15 @@
-INSERT INTO visits (visitor_name) VALUES ('John');
-INSERT INTO visits (visitor_name) VALUES ('Mark');
-INSERT INTO visits (visitor_name) VALUES ('Robin');
+INSERT INTO Design (design_id, text_number, height, width) VALUES ();
+INSERT INTO Design (design_id, text_number, height, width) VALUES ();
+
+INSERT INTO Text (design_id, letter_nuber, usage,
+ left_location, up_location, width, letter_color,
+  letter_size, highlight_size, highlight_color, highlight_font) 
+VALUES ();
+INSERT INTO Text (design_id, letter_nuber, usage,
+ left_location, up_location, width, letter_color,
+  letter_size, highlight_size, highlight_color, highlight_font) 
+VALUES ();
+INSERT INTO Text (design_id, letter_nuber, usage,
+ left_location, up_location, width, letter_color,
+  letter_size, highlight_size, highlight_color, highlight_font) 
+VALUES ();
