9158e91a4b575710c4667c5f82be9ff1038f17d8
KwonJunYeop <201802051@o.cnu.ac.kr>
2023-03-23 23:32:06 +0900

    :sparkles: get, post 적용

--- backend/firstapp/models.py
+++ backend/firstapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Review(models.Model):
+    title = models.CharField(max_length=50) # title 컬럼
+    content = models.TextField()             # content 컬럼
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.title
 # Create your models here.
