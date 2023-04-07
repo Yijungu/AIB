2253f2bc3a9df79044a94a0c54eb37a8278192a1
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2023-04-07 23:33:54 +0900

    :sparkles: submit, example 페이지 구분

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FirstPage from "../page/first";
+import ExamplePage from "../page/first/examplePage";
+import SubmitPage from "../page/first/submitPage";
 import LastPage from "../page/last";
 import SecondPage from "../page/second";
 
@@ -8,7 +9,9 @@ const Router = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<FirstPage />} />
+        <Route path="/" element={<ExamplePage />} />
+        <Route path="/submit" element={<SubmitPage />} />
+        <Route path="/example" element={<ExamplePage />} />
         <Route path="/second" element={<SecondPage />} />
         <Route path="/last" element={<LastPage />} />
       </Routes>
