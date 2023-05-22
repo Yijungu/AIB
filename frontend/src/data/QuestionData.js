ee01c769ad8e5d3654ad739984836686f14f7354
PoeySK <piz115@naver.com>
2023-05-22 22:49:31 +0900

    :ambulance: 질문 데이터 변경 및 색깔 선택 삭제

--- frontend/src/data/QuestionData.js
+++ frontend/src/data/QuestionData.js
@@ -1,12 +1,19 @@
 export const questionData = [
   {
-    id: "first-questionData-id",
-    question: "1. 구체적인 배너의 주제(컨셉)은 무엇인가요?",
+    id: "one-questionData-id",
+    question:
+      "1. 배너를 통해 광고하고자 하는 핵심 키워드(주제, 단어)는 무엇인가요?",
     ph: "ex) 여름 휴가를 위한 여름 여행 상품 홍보",
   },
   {
-    id: "second-questionData-id",
-    question: "2. 배너에 포함되었으면 하는 이미지는 어떤 것이 있나요?",
-    ph: "ex) 여름 분위기의 아이콘, 모래, 바다, 해양 스포츠",
+    id: "two-questionData-id",
+    question: "2. 제작하고 싶은 배너의 크기를 지정해주세요.",
+    ph: "",
+  },
+  {
+    id: "three-questionData-id",
+    question:
+      "3. 배너에 포함시키고 싶은 문구가 있으면 해당 문구를 작성 및 문구의 용도를 선택해주세요.",
+    ph: "",
   },
 ];
