9b6f9f27df4b1094fb7dad1d62626444be09f346
PoeySK <piz115@naver.com>
2023-05-22 22:28:04 +0900

    :sparkles: 이미지 GET으로 받아오기

--- frontend/src/page/finallyImg/index.js
+++ frontend/src/page/finallyImg/index.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { ImgWithEditableText } from "../../components/imgWithEditableText/imgWithEditableText";
 
 const LastPage = () => {
+  const [getImageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    // 이미지를 받아올 API 엔드포인트 호출
+    fetchImage()
+      .then((response) => {
+        // 응답 데이터에서 이미지 URL 추출
+        const getImageUrl = response.data.getImageUrl;
+        // 이미지 URL을 상태 변수에 저장
+        setImageUrl(getImageUrl);
+      })
+      .catch((error) => {
+        // 오류 처리
+        console.error("Failed to fetch image:", error);
+      });
+  }, []);
+
+  const fetchImage = async () => {
+    try {
+      const response = await axios.get("http://localhost/api/image");
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to fetch image");
+    }
+  };
+
   const image = {
-    imageUrl: "/testImgJpeg.jpeg",
+    // imageUrl: getImageUrl,
+    imageUrl: "./testImgJpeg.jpeg",
     initialTexts: [
       { text: "AIB Good Service", fontSize: 24, fontFamily: "Arial" },
       { text: "AIB Project Finish!!!", fontSize: 18, fontFamily: "Arial" },
