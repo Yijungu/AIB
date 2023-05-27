fb8b3a6d30584109bcc176dd23d203a66b7ce55d
PoeySK <piz115@naver.com>
2023-05-27 16:55:37 +0900

    :sparkles: 다운로드 버튼 생성

--- frontend/src/components/menu/topMenu/topMenu.jsx
+++ frontend/src/components/menu/topMenu/topMenu.jsx
@@ -1,13 +1,28 @@
-import { React } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./topMenu.css";
+import imageUrl from "../../../page/finallyImg/index";
 
 export const TopMenu = () => {
+  const handleDownloadImage = () => {
+    const downloadLink = document.createElement("a");
+    downloadLink.href = imageUrl;
+    downloadLink.download = "image.jpg";
+    downloadLink.click();
+  };
+
   return (
     <nav>
       <ul>
         <li id="text-box">
-          <Link id="aib-logo">AIB</Link>
+          <Link id="aib-logo" to="/">
+            AIB
+          </Link>
+        </li>
+        <li>
+          <button className="download-btn" onClick={handleDownloadImage}>
+            Download Image
+          </button>
         </li>
       </ul>
     </nav>
--- frontend/src/page/finallyImg/index.js
+++ frontend/src/page/finallyImg/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./index.css";
+import { TopMenu } from "../../components/menu/topMenu/topMenu";
 
 const LastPage = () => {
+  const imageUrl = "./image.jpeg";
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleSidebar = () => {
@@ -13,27 +15,32 @@ const LastPage = () => {
   };
 
   return (
-    <div id="last-page">
-      <div className={`image-container ${isSidebarOpen ? "shift-right" : ""}`}>
-        <img
-          src="./image.jpeg"
-          className="image"
-          alt="Main Image"
-          onClick={handleImageClick}
-        />
-      </div>
-      {isSidebarOpen && (
-        <div className="sidebar">
-          <button className="close-btn" onClick={toggleSidebar}>
-            Close Sidebar
-          </button>
-          <div className="content">
-            <h2>Sidebar Content</h2>
-            <p>This is the content of the sidebar.</p>
-          </div>
+    <>
+      <TopMenu imageUrl={imageUrl} /> {/* imageUrl을 TopMenu로 전달 */}
+      <div id="last-page">
+        <div
+          className={`image-container ${isSidebarOpen ? "shift-right" : ""}`}
+        >
+          <img
+            src={imageUrl}
+            className="image"
+            alt="Main Image"
+            onClick={handleImageClick}
+          />
         </div>
-      )}
-    </div>
+        {isSidebarOpen && (
+          <div className="sidebar">
+            <button className="close-btn" onClick={toggleSidebar}>
+              Close Sidebar
+            </button>
+            <div className="content">
+              <h2>Sidebar Content</h2>
+              <p>This is the content of the sidebar.</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
