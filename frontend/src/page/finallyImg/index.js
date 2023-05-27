a34c73202d075bc1d959100558e79ea85a1bb947
PoeySK <piz115@naver.com>
2023-05-27 16:13:39 +0900

    :ambulance: 사이드바 수정

--- frontend/src/page/finallyImg/index.js
+++ frontend/src/page/finallyImg/index.js
@@ -9,28 +9,30 @@ const LastPage = () => {
   };
 
   const handleImageClick = () => {
-    if (!isSidebarOpen) {
-      setIsSidebarOpen(true);
-    }
+    setIsSidebarOpen(true);
   };
 
   return (
     <div id="last-page">
-      <div
-        className={`image-container ${isSidebarOpen ? "shift-right" : ""}`}
-        onClick={handleImageClick}
-      >
-        <img src="./image.jpeg" className="image" alt="Main Image" />
+      <div className={`image-container ${isSidebarOpen ? "shift-right" : ""}`}>
+        <img
+          src="./image.jpeg"
+          className="image"
+          alt="Main Image"
+          onClick={handleImageClick}
+        />
       </div>
-      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
-        <button className="close-btn" onClick={toggleSidebar}>
-          Close Sidebar
-        </button>
-        <div className="content">
-          <h2>Sidebar Content</h2>
-          <p>This is the content of the sidebar.</p>
+      {isSidebarOpen && (
+        <div className="sidebar">
+          <button className="close-btn" onClick={toggleSidebar}>
+            Close Sidebar
+          </button>
+          <div className="content">
+            <h2>Sidebar Content</h2>
+            <p>This is the content of the sidebar.</p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
