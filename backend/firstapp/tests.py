8d7b0d1e56f348dc912e21be90f89def85e73a58
KwonJunYeop <201802051@o.cnu.ac.kr>
2023-09-15 22:15:04 +0900

    :zap: template 다시 db에 저장하고 돌려보기

--- backend/firstapp/tests.py
+++ backend/firstapp/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase, Client
 import json
 import base64
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 class RequestViewTestCase(TestCase):
 
     def setUp(self):
         self.client = Client()
 
-    def test_request_view_with_pictures(self):
+    """def test_request_view_with_pictures(self):
         data = {
             'concept': 'some_concept',
             'include': 'some_include',
@@ -14,27 +16,28 @@ class RequestViewTestCase(TestCase):
                 {'comment': 'comment1', 'select': 'select1'},
                 {'comment': 'comment2', 'select': 'select2'}
             ]),
-            'logo': open('sam-removebg-preview.png', 'rb'),
-            'product': open('phone-removebg-preview.png', 'rb')
         }
-            
-        response = self.client.post('/aib_request/', data)
+
+        data['logo'] = SimpleUploadedFile(name='sam-removebg-preview.png', content=open('sam-removebg-preview.png', 'rb').read())
+        data['product'] = SimpleUploadedFile(name='phone-removebg-preview.png', content=open('phone-removebg-preview.png', 'rb').read())
+
+        response = self.client.post('/aib_request/', data, format='multipart')
         self.assertEqual(response.status_code, 200)
         self.assertIn('background_color', response.json())
-        self.assertIn('text_color', response.json())
+        self.assertIn('text_color', response.json())"""
 
 
     def test_request_view_without_pictures(self):
         data = {
             'concept': 'some_concept',
-            'include': 'some_include',
-            'contents': [
-                {'comment': 'comment1', 'select': 'select1'},
-                {'comment': 'comment2', 'select': 'select2'}
-            ]
+            'include': '1200:360',
+            'contents': json.dumps([
+                {'comment': 'comment1', 'select': '큰 홍보문구'},
+                {'comment': 'comment2', 'select': '작은 홍보문구'}
+            ], ensure_ascii=False),
         }
 
-        response = self.client.post('/aib_request/', json.dumps(data), content_type='application/json')
+        response = self.client.post('/aib_request/', data, content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
         self.assertIn('image', response.json())
