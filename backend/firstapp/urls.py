9e38a91face05fd5f2b2c2c73b1bf53942de6b71
Yijungu <wnsrn4970>
2023-11-16 21:32:37 +0900

    경량화

--- backend/firstapp/urls.py
+++ backend/firstapp/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('example/', views.example_view, name='example_view'),
-    path('test/', views.test_view, name='test'),
-    path('api/request_view/', views.request_view, name='request_view'),  # 일반 요청 처리
-    path('api/request_picture_view/', views.request_picture_view, name='request_picture_view'),  # 이미지 포함 요청 처리
+    # path('example/', views.example_view, name='example_view'),
+    # path('test/', views.test_view, name='test'),
+    # path('api/request_view/', views.request_view, name='request_view'),  # 일반 요청 처리
+    # path('api/request_picture_view/', views.request_picture_view, name='request_picture_view'),  # 이미지 포함 요청 처리
     # path('api/', include('api.urls')),
 
     # 사용자가 아무것도 없는 경로로 들어온 경우
